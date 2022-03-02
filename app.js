ee1b05d7019a4fe25373295254887f3ea444c88f
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-03-02 16:31:30 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const request = require("request");
+
+const url =
+  "http://api.weatherstack.com/current?access_key=4a45146f35fdaf16a54a48a61c9a3ec9&query=Sydney";
+
+request({ url: url, json: true }, (error, response) => {
+  //   console.log(response.body.current);
+  console.log(
+    response.body.current.weather_descriptions[0] +
+      ". It is currently " +
+      response.body.current.temperature +
+      " degrees out. It feels like " +
+      response.body.current.feelslike +
+      " degrees out."
+  );
+});
